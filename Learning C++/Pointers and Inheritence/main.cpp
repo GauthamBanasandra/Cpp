e1373385df4183017ad2bfb8920d9f1474fb40cc
Gautham Banasandra <gautham.bangalore@gmail.com>
2017-05-10 20:15:35 +0530

    Polymorhphism with references.

--- Learning C++/Pointers and Inheritence/main.cpp
+++ Learning C++/Pointers and Inheritence/main.cpp
@@ -6,16 +6,24 @@ int main()
 {
 	Twitterati me("Jack", "@captain_jack", 12);
 	// Calls GetName() of Twitterati - obvious.
-	std::cout << "name:\t" << me.GetName() << std::endl;
+	std::cout << "solid obj - name:\t" << me.GetName() << std::endl;
 	
-	Person myself = me;
-	std::cout << "name:\t" << myself.GetName() << std::endl;
+	// Polymorphism - assigning a sub-class reference to a super-class.
+	Person *myself = new Twitterati("Jack", "@captain_jack", 12);
+	// GetName() of super-class is marked as 'virtual'. Thus, it calls the GetName() of sub-class.
+	// Remember - virtual works only if the calling object(e.g. myself) is a pointer/reference. It won't work if the calling object is a non pointer/reference.
+	std::cout << "ptr - name:\t" << myself->GetName() << std::endl;
+	delete myself;
+
+	// Polymorphic reference.
+	Person &myselfRef = me;
+	std::cout << "ref - name:\t" << myselfRef.GetName() << std::endl;
 
 	// Inheritence with shared_ptd.
 	// LHS should have parent class and RHS should have the child class type.
 	std::shared_ptr<Person> s_me = std::make_shared<Twitterati>("Jack", "@captain_jack", 12);
 	// Since 'GetName()' function is marked as virutal, it will call GetName() of sub class.
-	std::cout << "name:\t" << s_me->GetName() << std::endl;
+	std::cout << "shared_ptr - name:\t" << s_me->GetName() << std::endl;
 
 	return 0;
 }
