5fa893a72d13dbe8b878998b6b9225441dc65661
Gautham Banasandra <gautham.bangalore@gmail.com>
2017-05-14 20:23:52 +0530

    Capture clause.

--- Learning C++/Lambda/main.cpp
+++ Learning C++/Lambda/main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char* argv[])
 	// Printing the numbers using lambda.
 	std::for_each(numbers.begin(), numbers.end(), [](int i) {std::cout << i << " "; });
 	std::cout << std::endl << std::endl;
-	
+
 	std::cout << "Odd/even:" << std::endl;
 	// Printing odd/even using lambda.
 	std::for_each(numbers.begin(), numbers.end(), [](int num)
@@ -44,7 +44,54 @@ int main(int argc, char* argv[])
 	std::transform(numbers.begin(), numbers.end(), std::back_inserter(squares), [](int n)->double {return sqrt(n); });
 	std::cout << "Square root:" << std::endl;
 	std::for_each(squares.begin(), squares.end(), [](double n)->void {std::cout << n << " "; });
-	std::cout << std::endl;
+	std::cout << std::endl << std::endl;
 
+	// The capture clause in lambda [].
+	// It specifies the variables in the calling scope that will be used in lambda.
+	int x = 2, y = 10;
+	numbers.clear();
+	for (int i = 0; i < NUM; ++i)
+	{
+		numbers.push_back(i);
+	}
+	std::cout << "Numbers between " << x << " and " << y << ':' << std::endl;
+	// Mention x, y in [] to be used in the lambda.
+	std::for_each(numbers.begin(), numbers.end(), [x, y](int n)
+	{
+		if (n > x && n < y)
+		{
+			std::cout << n << " ";
+		}
+	});
+	std::cout << std::endl << std::endl;
+
+	// Use[=] to capture all the variables in the calling function stack.
+	x = 5;
+	y = 9;
+	std::cout << "Numbers between " << x << " and " << y << ':' << std::endl;
+	// Mention x, y in [] to be used in the lambda.
+	std::for_each(numbers.begin(), numbers.end(), [=](int n)
+	{
+		if (n > x && n < y)
+		{
+			std::cout << n << " ";
+		}
+	});
+	std::cout << std::endl << std::endl;
+
+	std::cout << "Doubling the numbers:" << std::endl;
+	// The parameter that is passed to lambda could be a reference too.
+	std::for_each(numbers.begin(), numbers.end(), [](int &n)
+	{
+		n *= 2;
+	});
+	std::for_each(numbers.begin(), numbers.end(), [](int n)
+	{
+		std::cout << n << ' ';
+	});
+	std::cout << std::endl << std::endl;
+
+	// Even the capture clause could be reference. e.g. [&x, &y]. Any change to x and y will be reflected in the calling scope.
+	// To capture the references of all the variables in the stack, use [&] as the capture clause.
 	return 0;
 }
