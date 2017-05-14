c656487ae031ddec55c721ef970125155f8def80
Gautham Banasandra <gautham.bangalore@gmail.com>
2017-05-14 07:48:49 +0530

    Returning from a lambda.

--- Learning C++/Lambda/main.cpp
+++ Learning C++/Lambda/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <algorithm>
 #include <iostream>
+#include <math.h>
 
 int main(int argc, char* argv[])
 {
@@ -20,7 +21,8 @@ int main(int argc, char* argv[])
 	// Printing the numbers using lambda.
 	std::for_each(numbers.begin(), numbers.end(), [](int i) {std::cout << i << " "; });
 	std::cout << std::endl << std::endl;
-
+	
+	std::cout << "Odd/even:" << std::endl;
 	// Printing odd/even using lambda.
 	std::for_each(numbers.begin(), numbers.end(), [](int num)
 	{
@@ -33,6 +35,16 @@ int main(int argc, char* argv[])
 			std::cout << "Even:\t" << num << std::endl;
 		}
 	});
+	std::cout << std::endl;
+
+	// Squaring the numbers.
+	std::vector<double> squares;
+	// Using transform from STL. back_inserter inserts at the end of the collection.
+	// The return type of a lambda is specified using the '->' operator.
+	std::transform(numbers.begin(), numbers.end(), std::back_inserter(squares), [](int n)->double {return sqrt(n); });
+	std::cout << "Square root:" << std::endl;
+	std::for_each(squares.begin(), squares.end(), [](double n)->void {std::cout << n << " "; });
+	std::cout << std::endl;
 
 	return 0;
 }
